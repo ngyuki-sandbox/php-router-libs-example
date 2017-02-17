e4c88ce86aab76c0e600d8db485befb8094a876f
ngyuki <ngyuki.jp@gmail.com>
2017-02-17 21:33:19 +0900

    rename fast.php

--- example/fast.php
+++ example/fast.php
@@ -1,7 +1,7 @@
 <?php
 namespace App;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use function FastRoute\simpleDispatcher;
 use FastRoute\RouteCollector;
